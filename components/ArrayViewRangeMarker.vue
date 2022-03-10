6822a22ab9471c6102abd9d9c0ec5b6377790986
Raku Zeta <zetaraku@gmail.com>
2022-03-10 17:35:00 +0800

    feat: add slot support for ArrayViewRangeMarker

--- components/ArrayViewRangeMarker.vue
+++ components/ArrayViewRangeMarker.vue
@@ -10,10 +10,15 @@ const props = defineProps<{
     class="
       absolute w-15 h-15 border-4 -ml-1
       transition-all duration-300
+      inline-flex justify-center items-center
     "
     :style="{
       width: `${Math.max(0, (-4) + 4 * (15 + 1) * (end - begin))}px`,
       transform: `translateX(${4 * (15 + 1) * begin}px)`,
     }"
-  />
+  >
+    <slot>
+      <!-- marker info -->
+    </slot>
+  </div>
 </template>
