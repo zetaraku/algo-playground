580744cee8ef5657e2f676615d168e6af132ccfa
Raku Zeta <zetaraku@gmail.com>
2022-03-10 19:28:39 +0800

    feat: add ArrayViewIndexMarker

---
+++ components/ArrayViewIndexMarker.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  index: number,
+}>();
+</script>
+
+<template>
+  <ArrayViewRangeMarker
+    :begin-index="index"
+    :end-index="index + 1"
+  >
+    <slot>
+      <!-- marker info -->
+    </slot>
+  </ArrayViewRangeMarker>
+</template>
