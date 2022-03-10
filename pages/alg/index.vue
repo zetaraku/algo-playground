3681af9db565404bae5496b08004f85bb78506b0
Raku Zeta <zetaraku@gmail.com>
2022-03-10 22:02:51 +0800

    feat: add alg index page

---
+++ pages/alg/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const categories = [
+  {
+    title: 'Strings',
+    pages: [
+      { title: 'KMP algorithm', to: '/alg/kmp' },
+    ],
+  },
+];
+</script>
+
+<template>
+  <div>
+    <h1>Algorithm</h1>
+    <ul class="text-6">
+      <li
+        v-for="(category, i) in categories"
+        :key="i"
+      >
+        {{ category.title }}
+        <ul>
+          <li
+            v-for="(page, j) in category.pages"
+            :key="j"
+          >
+            <NuxtLink :to="page.to">
+              {{ page.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
