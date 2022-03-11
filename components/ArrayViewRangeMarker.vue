e331a08b1d661b346404be67a0e391be25c53253
Raku Zeta <zetaraku@gmail.com>
2022-03-11 10:55:21 +0800

    chore: use left instead of transform for style

--- components/ArrayViewRangeMarker.vue
+++ components/ArrayViewRangeMarker.vue
@@ -8,13 +8,13 @@ const props = defineProps<{
 <template>
   <div
     class="
-      absolute w-15 h-15 border-4 -ml-1
+      absolute w-15 h-15 border-4
       transition-all duration-300
       inline-flex justify-center items-center
     "
     :style="{
       width: `${Math.max(0, 4 + 4 * (15 - 1) * (endIndex - beginIndex))}px`,
-      transform: `translateX(${4 * (15 - 1) * beginIndex}px)`,
+      left: `${4 * (15 - 1) * beginIndex}px`,
     }"
   >
     <slot>
