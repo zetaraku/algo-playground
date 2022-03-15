3cd9fbf056f3e5c48df9fa38fc57f9b89d247796
Raku Zeta <zetaraku@gmail.com>
2022-03-15 13:59:53 +0800

    chore: drop use of @unocss/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,26 +6,6 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
   },
-  buildModules: [
-    '@unocss/nuxt',
-  ],
   css: [
-    '@unocss/reset/sanitize/sanitize.css',
   ],
-  unocss: {
-    autoImport: true, // inject `uno.css`
-    preflight: false, // inject `@unocss/reset/tailwind.css`
-    components: true, // install UnoCSS components
-
-    // presets
-    uno: true, // enabled `@unocss/preset-uno`
-    attributify: false, // enabled `@unocss/preset-attributify`
-    icons: true, // enabled `@unocss/preset-icons`
-    webFonts: true, // enabled `@unocss/preset-web-fonts`
-    typography: true, // enabled `@unocss/preset-typography`
-
-    // core options
-    shortcuts: [],
-    rules: [],
-  },
 });
