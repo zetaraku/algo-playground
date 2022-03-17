361d5f0d8fc9bfd80e5bdd291ea426940d3bb9c0
Raku Zeta <zetaraku@gmail.com>
2022-03-17 14:33:41 +0800

    feat: ArrayViewItem can now display its value

--- components/ArrayViewItem.vue
+++ components/ArrayViewItem.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value?: any,
+}>();
+</script>
+
 <template>
   <div
     class="d-inline-flex justify-content-center align-items-center fs-2"
@@ -8,6 +14,23 @@
       vertical-align: middle;
     "
   >
-    <slot />
+    <slot :value="value">
+      <span
+        v-if="value === undefined"
+        style="color: darkgray;"
+      >
+        {{ '-' }}
+      </span>
+      <span
+        v-else-if="value === null"
+        class="fs-6"
+        style="color: blue;"
+      >
+        {{ 'NULL' }}
+      </span>
+      <span v-else>
+        {{ value }}
+      </span>
+    </slot>
   </div>
 </template>
