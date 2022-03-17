924ec5c1b0aa962dbbd8fef9a9f3c0b3eb0edf55
Raku Zeta <zetaraku@gmail.com>
2022-03-17 12:42:59 +0800

    refactor: reimplement KeyedArrayView with KeyedMatrixView

--- components/KeyedArrayView.vue
+++ components/KeyedArrayView.vue
@@ -5,87 +5,27 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div
-    class="position-relative"
-    style="padding-bottom: 60px;"
-  >
+  <KeyedMatrixView :data="[data]">
     <!-- array elements -->
-    <TransitionGroup name="list">
-      <ArrayViewItem
-        v-for="(item, index) in data"
-        :key="item.key"
-        class="position-absolute"
-        :style="{
-          left: `${4 * (15 - 1) * index}px`,
-        }"
-      >
-        <slot
-          name="item"
-          :index="index"
-          :value="item.value"
-          :item="item"
-        >
-          <span
-            v-if="item.value === undefined"
-            style="color: darkgray;"
-          >
-            {{ '-' }}
-          </span>
-          <span
-            v-else-if="item.value === null"
-            class="fs-6"
-            style="color: blue;"
-          >
-            {{ 'NULL' }}
-          </span>
-          <span v-else>
-            {{ item.value }}
-          </span>
-        </slot>
-      </ArrayViewItem>
-    </TransitionGroup>
+    <template #item="{ jIndex, value, item }">
+      <slot
+        name="item"
+        :index="jIndex"
+        :value="value"
+        :item="item"
+      />
+    </template>
 
     <!-- end indicator -->
-    <ArrayViewItem
-      class="position-absolute"
-      style="background-color: lightgray;"
-      :style="{ left: `${4 * (15 - 1) * data.length}px` }"
-    >
+    <template #end="{ jIndex }">
       <slot
         name="end"
-        :index="data.length"
-      >
-        <span
-          class="fs-5"
-          style="transform: rotate(-45deg);"
-        >
-          END
-        </span>
-      </slot>
-    </ArrayViewItem>
+        :index="jIndex"
+      />
+    </template>
 
     <slot>
       <!-- marker slot -->
     </slot>
-  </div>
+  </KeyedMatrixView>
 </template>
-
-<style lang="scss" scoped>
-.list-move, /* apply transition to moving elements */
-.list-enter-active,
-.list-leave-active {
-  transition: all 0.5s ease;
-}
-
-.list-enter-from,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-
-/* ensure leaving items are taken out of layout flow so that moving
-   animations can be calculated correctly. */
-.list-leave-active {
-  position: absolute;
-}
-</style>
