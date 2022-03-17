7dac8660caba09041cabb535764b26e4a9680c26
Raku Zeta <zetaraku@gmail.com>
2022-03-17 14:30:10 +0800

    feat: add ArrayViewEnd component

---
+++ components/ArrayViewEnd.vue
@@ -0,0 +1,14 @@
+<template>
+  <ArrayViewItem
+    style="background-color: lightgray;"
+  >
+    <slot>
+      <span
+        class="fs-5"
+        style="transform: rotate(-45deg);"
+      >
+        END
+      </span>
+    </slot>
+  </ArrayViewItem>
+</template>
