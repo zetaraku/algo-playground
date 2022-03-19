106c8730f7fe86ba9f39fff269e36cccc054d193
Raku Zeta <zetaraku@gmail.com>
2022-03-19 13:55:13 +0800

    feat: add MatrixViewIndexMarker

---
+++ components/MatrixViewIndexMarker.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  index: { i: number, j: number },
+}>();
+</script>
+
+<template>
+  <MatrixViewRangeMarker
+    :begin-index="{ i: index.i, j: index.j }"
+    :end-index="{ i: index.i + 1, j: index.j + 1 }"
+  >
+    <slot>
+      <!-- marker info -->
+    </slot>
+  </MatrixViewRangeMarker>
+</template>
