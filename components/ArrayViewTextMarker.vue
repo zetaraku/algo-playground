032e65d711861ee907a22d8319e5646ef2b4f8d4
Raku Zeta <zetaraku@gmail.com>
2022-03-19 19:43:27 +0800

    feat: add ArreyViewTextMarker

---
+++ components/ArrayViewTextMarker.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  index: number,
+}>();
+</script>
+
+<template>
+  <MatrixViewIndexMarker
+    class="marker-color-text-only"
+    :index="{ i: 1, j: index }"
+  >
+    <div
+      class="position-absolute translate-middle text-nowrap"
+      style="top: 50%; left: 50%;"
+    >
+      <slot>
+        <!-- marker info -->
+      </slot>
+    </div>
+  </MatrixViewIndexMarker>
+</template>
+
+<style lang="scss" scoped>
+.marker-color-text-only {
+  border-color: transparent !important;
+  background-color: transparent !important;
+}
+</style>
