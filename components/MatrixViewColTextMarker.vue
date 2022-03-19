dc028b1029bae1ede55192dc657dbed53f5c5168
Raku Zeta <zetaraku@gmail.com>
2022-03-19 16:56:34 +0800

    feat: add MatrixViewRowTextMarker and MatrixViewColTextMarker

---
+++ components/MatrixViewColTextMarker.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  colIndex: number,
+}>();
+
+const rowCount = inject<number>('rowCount');
+</script>
+
+<template>
+  <MatrixViewIndexMarker
+    class="marker-color-text-only"
+    :index="{ i: rowCount! + 1, j: colIndex }"
+  >
+    <div
+      class="position-absolute translate-middle text-nowrap"
+      style="top: 50%; left: 50%;"
+    >
+      <slot>
+        <!-- marker info -->
+      </slot>
+    </div>
+  </MatrixViewIndexMarker>
+</template>
+
+<style lang="scss" scoped>
+.marker-color-text-only {
+  border-color: transparent !important;
+  background-color: transparent !important;
+}
+</style>
---
+++ components/MatrixViewRowTextMarker.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  rowIndex: number,
+}>();
+
+const colCount = inject<number>('colCount');
+</script>
+
+<template>
+  <MatrixViewIndexMarker
+    class="marker-color-text-only"
+    :index="{ i: rowIndex, j: colCount! + 1 }"
+  >
+    <div
+      class="position-absolute translate-middle-y text-nowrap"
+      style="top: 50%; left: 25%;"
+    >
+      <slot>
+        <!-- marker info -->
+      </slot>
+    </div>
+  </MatrixViewIndexMarker>
+</template>
+
+<style lang="scss" scoped>
+.marker-color-text-only {
+  border-color: transparent !important;
+  background-color: transparent !important;
+}
+</style>
