8df479af5305c92c1ddb28fd00321675407764fd
Raku Zeta <zetaraku@gmail.com>
2022-03-20 12:32:00 +0800

    feat: refactor useProcess composable

--- composables/useProcess.ts
+++ composables/useProcess.ts
@@ -1,8 +1,9 @@
 export default function useProcess() {
   const currentProcess = ref<Generator<string, string> | null>(null);
   const infoMessage = ref<string | null>(null);
-  const autoPlaying = ref<boolean>(false);
   const autoPlayDelay = ref<number>(500);
+  const autoplayTimeoutId = ref<number | null>(null);
+  const autoPlaying = computed<boolean>(() => autoplayTimeoutId.value !== null);
 
   function nextStep() {
     if (currentProcess.value === null) return;
@@ -12,19 +13,42 @@ export default function useProcess() {
 
     if (result.done) currentProcess.value = null;
   }
-  async function autoPlay() {
-    if (autoPlaying.value) return;
 
-    autoPlaying.value = true;
-    while (currentProcess.value !== null) {
-      nextStep();
-      // eslint-disable-next-line no-await-in-loop
-      await new Promise((resolve) => {
-        setTimeout(resolve, autoPlayDelay.value);
-      });
-    }
-    autoPlaying.value = false;
+  function stopAutoplay() {
+    if (autoplayTimeoutId.value === null) return;
+
+    window.clearTimeout(autoplayTimeoutId.value);
+    autoplayTimeoutId.value = null;
   }
+
+  function autoplayLoop() {
+    if (currentProcess.value === null) {
+      stopAutoplay();
+      return;
+    }
+
+    autoplayTimeoutId.value = window.setTimeout(
+      autoplayLoop,
+      autoPlayDelay.value,
+    );
+
+    nextStep();
+  }
+
+  function startAutoplay() {
+    if (autoplayTimeoutId.value !== null) return;
+
+    autoplayLoop();
+  }
+
+  function toggleAutoplay() {
+    if (autoPlaying.value) {
+      stopAutoplay();
+    } else {
+      startAutoplay();
+    }
+  }
+
   function skipAll() {
     while (currentProcess.value !== null) {
       nextStep();
@@ -32,10 +56,13 @@ export default function useProcess() {
   }
 
   function beginProcess(start: () => Generator<string, string>) {
+    stopAutoplay();
     currentProcess.value = start();
     nextStep();
   }
+
   function endProcess() {
+    stopAutoplay();
     currentProcess.value = null;
     infoMessage.value = null;
   }
@@ -48,7 +75,10 @@ export default function useProcess() {
     beginProcess,
     endProcess,
     nextStep,
-    autoPlay,
+    autoPlay: startAutoplay,
+    startAutoplay,
+    stopAutoplay,
+    toggleAutoplay,
     skipAll,
   };
 }
